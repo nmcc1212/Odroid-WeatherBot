8253b59bedadba02d9f92e3fa74317197f0647dc
nmcc1212 <nial538@gmail.com>
2020-09-02 13:08:09 +0100

    push

--- gpiotest.py
+++ gpiotest.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
+from time import sleep
 import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(19,out)
+GPIO.setup(19,GPIO.OUT)
 
-GPIO.output(19,GPIO.HIGH)
+GPIO.output(19,GPIO.LOW)
 
 sleep(5)
 
-GPIO.output(19,GPIO.LOW)
+GPIO.output(19,GPIO.HIGH)
+
+exit()
--- index.php
+++ index.php
@@ -8,31 +8,21 @@
          <form method="get" action="index.php">                
             <input type="submit" style = "font-size: 14 pt" value="OFF" name="off">
             <input type="submit" style = "font-size: 14 pt" value="ON" name="on">
-            <input type="submit" style = "font-size: 14 pt" value="Activate irrigation for 30 minutes" name="30mins">
          </form>​​​
                          </center>
 <?php
-   $pythonpath = getenv('PYTHONPATH');
-   $additional = '/usr/bin/python3';
-   if($pythonpath === false){
-   putenv("PYTHONPATH=$additional");
-   }
     shell_exec("/usr/bin/gpio -g mode 235 out");
     if(isset($_GET['off']))
         {
                         echo "Irrigation is off";
-                        shell_exec("/usr/bin/gpio -g write 235 0");
+                        $command = escapeshellcmd('/home/odroid/gpiotest.py');
+                        $output = shell_exec($command);
+                        echo $output;
         }
             else if(isset($_GET['on']))
             {
                         echo "Irrigation is on";
                         shell_exec("/usr/bin/gpio -g write 235 1");
-            }
-            else if(isset($_GET["30mins"]))
-            {
-                        
-                        shell_exec("/usr/bin/python3 /home/odroid/30min.py");
-                        echo "30 mins activated";
             }
 ?>
    </body>
